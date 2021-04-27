b595b6aed70b24836d3a898eae087584b46a8cfc
alexfortessp <83304405+alexfortessp@users.noreply.github.com>
2021-04-27 14:39:31 -0300

    Add files via upload

---
+++ y e x.py
@@ -0,0 +1,15 @@
+x=int(input("digite valor de x"))
+
+y=int(input("digite o valor de y"))
+
+if x=='2' and y=='3':
+      print('i')
+      elif x=='-8' and y=='5':
+         print('ii')
+        elif x=='-4' and y=='-3':
+           print('iii')
+          elif x=='6' and y=='-2':
+             print('iv')
+             elif x=='0 'and y='0':
+               print('origem')
+                          
